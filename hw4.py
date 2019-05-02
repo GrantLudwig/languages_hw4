12dd3aedc0640937040194edefc646f9792eeb67
unknown <grantludwig97@gmail.com>
2019-05-02 15:00:05 -0700

    driver complete sans generator function

--- hw4.py
+++ hw4.py
@@ -3,6 +3,7 @@
 #5/8/19
 
 import sys
+from functools import reduce
 
 class InvalidFile(Exception): pass
 
@@ -24,17 +25,19 @@ def createFractionList(fileName):
         raise InvalidFile
     return fracList
 
+#Computes the greatest common denominator
 def gcd(x, y):
     while y:
         x, y = y, x % y
     return x
 
+#Simplifies a fraction
 def simplify(fraction):
     num, den = fraction
     if num == 0:
         den = 1
     elif abs(num) == den:
-        num = abs(num)/num
+        num = int(abs(num)/num)
         den = 1
     else:
         commonDiv = gcd(num,den)
@@ -48,7 +51,7 @@ def genDifferences(fractionList, target):
 
 #Driver code
 fileName = sys.argv[1]
-target = (sys.argv[2], sys.argv[3])
+target = (int(sys.argv[2]), int(sys.argv[3]))
 try:
     fractionList = createFractionList(fileName)
 except FileNotFoundError:
@@ -58,9 +61,22 @@ except InvalidFile:
     print('Invalid file')
     sys.exit()
 print(fractionList)
+#Simplify fractions
 simFractions = [simplify(fraction) for fraction in fractionList]
 print(simFractions)
+##TODO Generator
 #for output in genDifferences(fractionList, target):
     #print(output)
-decimalValues = map((lambda x,y: x / y), fraction in fractionList)
+#Decimal Fractions
+decimalValues = list(map((lambda x: x[0] / x[1]), fractionList))
 print(decimalValues)
+#Double Fractions
+doubledFractions = list(map((lambda x: simplify((x[0]*2, x[1]))), fractionList))
+print(doubledFractions)
+#Fractions greater than target
+filterList = list(filter((lambda x: x[0]/x[1] > target[0]/target[1]), fractionList))
+print(filterList)
+#Minimum Fraction
+print(reduce(lambda x, y: x if x[0]/x[1] < y[0]/y[1] else y, fractionList))
+#Sum of fractions (in simpliest form)
+print(reduce(lambda x, y: simplify((x[0]*y[1] + y[0]*x[1],x[1]*y[1])), fractionList))
